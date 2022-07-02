c0b602274d352079e9e0d583b510f507bcfb8ca7
Kyle Hatch <ky13h4tch@gmail.com>
2022-07-02 17:36:47 -0600

    Added a tooltip component and used it on the skills icons

---
+++ myPortfolio/src/components/Tooltip.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="tooltip-container" @mouseover="hover = true" @mouseleave="hover = false">
+        <slot />
+        <div v-show="hover" class="tooltip">
+            <span class="text">{{ text }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            hover: false,
+        }
+    },
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
+
+<style scoped>
+.tooltip-container {
+    position: relative;
+    display: inline-block;
+}
+
+.tooltip-container:hover .tooltip {
+    opacity: 1;
+}
+
+.tooltip {
+    color: #ffffff;
+    text-align: center;
+    padding: 10px 0;
+    border-radius: 2px;
+
+    width: 120px;
+    bottom: 120%;
+    left: 50%;
+    margin-left: -60px;
+
+    opacity: 0;
+    transition: opacity 0.5s;
+
+    position: absolute;
+    z-index: 1;
+
+    background: #000000;
+}
+
+.text::after {
+    content: " ";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #000000 transparent transparent transparent;
+
+}
+</style>
--- myPortfolio/src/views/AboutView.vue
+++ myPortfolio/src/views/AboutView.vue
@@ -6,8 +6,10 @@
           <div v-if="post.title" class="title">{{ post.title }}</div>
           <div v-if="post.body" class="body">{{ post.body }}</div>
           <div class="skills-container">
-            <div class="skills-images" v-for="(image, index) in post.images" :key="index">
-              <img v-if="image" :src="'/src/assets/' + image">
+            <div class="skills-images" v-for="(key, value) in post.images" :key="index">
+              <Tooltip :text="value">
+                <img v-if="key" :src="'/src/assets/skills/' + key">
+              </Tooltip>
             </div>
           </div>
         </div>
@@ -18,21 +20,25 @@
 
 <script>
 import AboutMeComponent from '../components/AboutMeComponent.vue';
+import Tooltip from '../components/Tooltip.vue';
+
 export default {
   data() {
     return {
+      hover: false,
       posts: [
         {
-          id: 0, title: 'About Me', body: `My journey to UX/UI started at a young age when I would create designs in Microsoft Paint and write simple HTML/CSS on my parent's computer. Since then, I have studied Computer Science and have been working as a software engineer. I plan to transition full-time to UX/UI after I finish the program at The University of Utah. 
-          I am a strong believer in dynamic and scalable design. Anytime you can reuse components in different ways, it not only speeds up workflow but also keeps consistency throughout the project, which is great for user experience. As an engineer, I have experience collaborating closely with design teams to make sure we keep that in mind.` },
-        { id: 1, title: 'Skills', images: ['figma.png', 'photoshop.png', 'ai.png'] },
-        { id: 2, title: 'Hobbies', body: 'Hobbies' },
+          id: 0,
+          title: "About Me",
+          body: `My journey to UX/UI started at a young age when I would create designs in Microsoft Paint and write simple HTML/CSS on my parent's computer. Since then, I have studied Computer Science and have been working as a software engineer. I plan to transition full-time to UX/UI after I finish the program at The University of Utah. 
+          I am a strong believer in dynamic and scalable design. Anytime you can reuse components in different ways, it not only speeds up workflow but also keeps consistency throughout the project, which is great for user experience. As an engineer, I have experience collaborating closely with design teams to make sure we keep that in mind.`
+        },
+        { id: 1, title: "Skills", images: { "Figma": "figma.png", "Vue.js": "vue.png", "Photoshop": "photoshop.png", "Adobe Illustrator": "ai.png", "Wireframing": "wireframe.png" } },
+        { id: 2, title: "Hobbies", body: "Hobbies" },
       ]
-    }
+    };
   },
-  components: {
-    AboutMeComponent
-  }
+  components: { AboutMeComponent, Tooltip }
 }
 </script>
 
@@ -64,11 +70,17 @@ export default {
 .skills-container {
   display: flex;
   flex-direction: row;
-  gap: 20px;
+  gap: 35px;
 }
 
 img {
   height: 50px;
   width: 50px;
+  cursor: pointer;
+  transition: transform .2s;
+
+  &:hover {
+    transform: scale(1.2)
+  }
 }
 </style>
